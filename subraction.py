b3ac77a93b052ebb9ae70c99b5c526b0fcbd4aa3
Amogh2k5 <amogh.2k5@gmail.com>
2025-10-12 17:23:21 +0530

    Update subraction.py

--- subraction.py
+++ subraction.py
@@ -1,7 +1,6 @@
-def subtract(a, b):
-    return a-b
+
 
 # Example usage:
 x = int(input("Enter first number: "))
 y = int(input("Enter second number: "))
-print("Difference =", subtract(x, y))
+print("Difference =", x-y)
